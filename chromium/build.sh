4dfae54da66b6688bad97c4c14284b37fbf4c4d6
Vittorio Palmisano <vpalmisano@gmail.com>
2023-05-20 12:40:22 +0200

    Update chromium build script

--- chromium/build.sh
+++ chromium/build.sh
@@ -2,19 +2,21 @@
 set -ex
 
 export DIR=$(dirname $(realpath "${BASH_SOURCE:-$0}"))
-export BUILDDIR=${HOME}
+export BUILDDIR=${HOME}/chromium
+export CHROMIUM_SRC=${BUILDDIR}/src/chromium/src
 export PATCH_FILE=${DIR}/max-video-decoders.patch
+export PATH="$PATH:${BUILDDIR}/depot_tools"
 
 function setup() {
     sudo apt install -y gperf
     # https://chromium.googlesource.com/chromium/src/+/master/docs/linux/build_instructions.md
+    mkdir -p ${BUILDDIR}
     cd ${BUILDDIR}
     if [ ! -d depot_tools ]; then
         git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
     fi
-    export PATH="$PATH:${BUILDDIR}/depot_tools"
-    mkdir -p ${BUILDDIR}/chromium
-    cd ${BUILDDIR}/chromium
+    mkdir -p cd ${BUILDDIR}/src/chromium
+    cd ${BUILDDIR}/src/chromium
     fetch --nohooks chromium
     cd src
     ./build/install-build-deps.sh
@@ -54,39 +56,80 @@ treat_warnings_as_errors=false
 use_official_google_api_keys=false
 use_unofficial_version_number=false
 use_kerberos=false
+
+cc_wrapper="CCACHE_SLOPPINESS=time_macros ccache"
 EOF
 }
 
 function apply_patch() {
-    cd ${BUILDDIR}/chromium/src/third_party/webrtc
+    cd ${CHROMIUM_SRC}/third_party/webrtc
+    git checkout .
     git apply < ${PATCH_FILE}
     git diff --compact-summary
 }
 
 function remove_patch() {
-    cd ${BUILDDIR}/chromium/src/third_party/webrtc
-    git stash
+    cd ${CHROMIUM_SRC}/third_party/webrtc
+    git checkout .
 }
 
 function update() {
     remove_patch
-    cd ${BUILDDIR}/chromium/src
+    cd ${CHROMIUM_SRC}
+    git checkout .
     git switch main
     git pull origin main
-    git rebase-update
-    gclient sync -D
+    gclient sync -D --force --reset
     apply_patch
 }
 
 function build() {
-    cd ${BUILDDIR}/chromium/src
-    ionice -c3 nice -n19 autoninja -C out/Default "chrome/installer/linux:unstable_deb"
+    cd ${CHROMIUM_SRC}
+    time ionice -c3 nice -n19 autoninja -C out/Default "chrome/installer/linux:unstable_deb"
     mv out/Default/*.deb ${DIR}
 }
 
 function clean() {
-    cd ${BUILDDIR}/chromium/src
+    cd ${CHROMIUM_SRC}
     gn clean out/Default
+}
+
+#
+export BRANCH=5735
+export CEF_USE_GN=1
+export GN_DEFINES='cc_wrapper="CCACHE_SLOPPINESS=time_macros ccache" is_official_build=true proprietary_codecs=true ffmpeg_branding=Chrome use_gnome_keyring=false use_system_libdrm=false use_sysroot=true use_allocator=none symbol_level=1 is_cfi=false use_thin_lto=false'
+export CEF_ARCHIVE_FORMAT=tar.bz2
+
+function update_cef() {
+    #sudo ./src/chromium/src/build/install-build-deps.sh
+    cd ${BUILDDIR}
+    curl https://bitbucket.org/chromiumembedded/cef/raw/master/tools/automate/automate-git.py -o automate-git.py
+    python automate-git.py \
+        --download-dir=${BUILDDIR}/src \
+        --depot-tools-dir=${BUILDDIR}/depot_tools \
+        --branch=${BRANCH} \
+        --build-target=cefsimple \
+        --with-pgo-profiles \
+        --x64-build \
+        --no-debug-build \
+        --force-clean \
+        --no-build
+}
+
+function build_cef() {
+    apply_patch
+    cd ${BUILDDIR}
+    time ionice -c3 nice -n19 python automate-git.py \
+        --download-dir=${BUILDDIR}/src \
+        --depot-tools-dir=${BUILDDIR}/depot_tools \
+        --branch=${BRANCH} \
+        --build-target=cefsimple \
+        --with-pgo-profiles \
+        --x64-build \
+        --no-debug-build \
+        --no-update \
+        --force-build
+    cp src/chromium/src/cef/binary_distrib/*.tar.bz2 ${DIR}
 }
 
 $@
