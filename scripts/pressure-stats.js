adc356c7ff734f9e2e1bbb3b21ffca5fc23de18e
Vittorio Palmisano <vpalmisano@gmail.com>
2024-05-30 09:45:10 +0200

    fix PressureObserver script

--- scripts/pressure-stats.js
+++ scripts/pressure-stats.js
@@ -10,19 +10,21 @@ window.collectCpuPressure = () => {
   return cpuPressure.mean()
 }
 
-if ('PressureObserver' in window) {
-  const STATES = {
-    nominal: 0,
-    fair: 1,
-    serious: 2,
-    critical: 3,
+document.addEventListener('DOMContentLoaded', async () => {
+  if ('PressureObserver' in window) {
+    const STATES = {
+      nominal: 0,
+      fair: 1,
+      serious: 2,
+      critical: 3,
+    }
+    const observer = new window.PressureObserver(records => {
+      const lastRecord = records[records.length - 1]
+      // log(`Current CPU pressure: ${lastRecord.state}`)
+      cpuPressure.push(Date.now(), STATES[lastRecord.state])
+    })
+    observer.observe('cpu', { sampleInterval: 1000 }).catch(error => {
+      log(`Pressure observer error: ${error}`)
+    })
   }
-  const observer = new window.PressureObserver(records => {
-    const lastRecord = records[records.length - 1]
-    // log(`Current CPU pressure: ${lastRecord.state}`)
-    cpuPressure.push(Date.now(), STATES[lastRecord.state])
-  })
-  observer.observe('cpu', { sampleInterval: 1000 }).catch(error => {
-    log(`Pressure observer error: ${error}`)
-  })
-}
+})
