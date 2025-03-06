3d54cd0c1a1503bb242cd359115469eb7a604a79
Vittorio Palmisano <vpalmisano@gmail.com>
2025-03-06 15:32:46 +0100

    Update Chromium build branch to version 135.0.7049.6

--- chromium/build.sh
+++ chromium/build.sh
@@ -6,7 +6,7 @@ export BUILDDIR=${HOME}/chromium
 export CHROMIUM_SRC=${BUILDDIR}/src/chromium/src
 export PATH="$PATH:${BUILDDIR}/depot_tools"
 
-export DEFAULT_BRANCH="tags/134.0.6957.0"
+export DEFAULT_BRANCH="tags/135.0.7049.6"
 
 function setup() {
     which gperf || sudo apt install -y gperf
