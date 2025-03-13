a8b19568ab9495bbfd0bb24d2935e4de3604db9c
Vittorio Palmisano <vpalmisano@gmail.com>
2025-03-13 13:19:11 +0100

    Fix event listener in createStream method to use 'canplay' and handle playback errors in get-user-media.js

--- scripts/get-user-media.js
+++ scripts/get-user-media.js
@@ -138,7 +138,7 @@ webrtcperf.FakeStream = class {
   createStream() {
     return new Promise((resolve, reject) => {
       this.element.addEventListener(
-        'canplaythrough',
+        'canplay',
         () => {
           webrtcperf.log(`[FakeStream] Create fake ${this.kind} stream done`)
           resolve(this.element.captureStream())
@@ -153,7 +153,9 @@ webrtcperf.FakeStream = class {
         },
         { once: true },
       )
-      this.element.play()
+      this.element.play().catch(err => {
+        webrtcperf.log(`[FakeStream] Create fake ${this.kind} play error:`, err)
+      })
     })
   }
 
