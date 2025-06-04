13849e65ef45b0ee856fe0de2a89bc5f56b75f17
Vittorio Palmisano <vpalmisano@gmail.com>
2025-06-04 11:50:47 +0200

    Add Google Meet automation script to join meetings automatically

---
+++ examples/google-meet.js
@@ -0,0 +1,25 @@
+/* global webrtcperf */
+
+async function initGoogleMeet() {
+  const nameInput = await webrtcperf.getElement('input[aria-label="Your name"]', 200)
+  if (!nameInput) return
+  if (!nameInput.value) {
+    nameInput.value = webrtcperf.overrides.getParticipantName()
+  }
+  await window.keypressText('input[aria-label="Your name"]', ' \n', 100)
+  const joinButton =
+    (await webrtcperf.getElements('button', 200, false, 'Join now'))[0] ||
+    (await webrtcperf.getElements('button', 200, false, 'Ask to join'))[0]
+  if (!joinButton) return
+  joinButton.click()
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  if (!document.location.href.startsWith('https://meet.google.com/')) {
+    return
+  }
+  while (true) {
+    await initGoogleMeet()
+    await webrtcperf.sleep(2000)
+  }
+})
