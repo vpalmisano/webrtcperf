74b696efb4675158b8664e540a886922f6ccb443
Vittorio Palmisano <vpalmisano@gmail.com>
2025-06-10 11:39:38 +0200

    Update default branch version in build script to 139.0.7230.1

--- chromium/build.sh
+++ chromium/build.sh
@@ -6,7 +6,7 @@ export BUILDDIR=${HOME}/chromium
 export CHROMIUM_SRC=${BUILDDIR}/src/chromium/src
 export PATH="$PATH:${BUILDDIR}/depot_tools"
 
-export DEFAULT_BRANCH="tags/139.0.7220.1"
+export DEFAULT_BRANCH="tags/139.0.7230.1"
 
 function setup() {
     which gperf || sudo apt install -y gperf
