2ee4222d11baf4589160ba2d812c674853ccba54
Vittorio Palmisano <vittorio@miro.com>
2025-09-09 09:25:26 +0200

    chore: update .gitignore to include *.dmg and .DS_Store; modify throttleConfig checks to apply only on Linux

--- src/app.ts
+++ src/app.ts
@@ -108,7 +108,7 @@ export class Application extends EventEmitter {
       }
 
       // Network throttle.
-      if (config.throttleConfig) {
+      if (config.throttleConfig && process.platform === 'linux') {
         await startThrottle(config.throttleConfig)
       }
 
@@ -197,7 +197,7 @@ export class Application extends EventEmitter {
 
     await this.stats.stop()
 
-    if (this.config.throttleConfig) {
+    if (this.config.throttleConfig && process.platform === 'linux') {
       await stopThrottle()
     }
 
